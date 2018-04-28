a8b7c337bd0cfe96028f78abea7436fecb7f0dbe
Angel Ricardo Vega Hernandez <angelricardo.uthh@gmail.com>
2018-04-27 19:42:49 -0500

    Initial commit

---
+++ src/main/java/jnotify/java_jnotify/App.java
@@ -0,0 +1,13 @@
+package jnotify.java_jnotify;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
